1777dcb3a18611397d363d747f52037fccf8ce7d
Dante-24 <62325969+Dante-24@users.noreply.github.com>
2022-05-14 06:30:59 +0530

    Add files via upload

---
+++ nqueens.py
@@ -0,0 +1,55 @@
+import sys
+sys.stdin = open("input.txt", "r")
+sys.stdout = open("output.txt", "w")
+N = int(input())
+
+def printsoln(board):
+	for i in range(N):
+		for j in range(N):
+			print(board[i][j], end=" ")
+		print()
+
+def issafe(row, col, slc, bslc, rlook, slclook, bslclook):
+	if slclook[slc[row][col]] or bslclook[bslc[row][col]] or rlook[row]:
+		return False
+	return True
+
+def panda(board, col, slc, bslc, rlook, slclook, bslclook):
+	if col >= N:
+		return True
+
+	for i in range(N):
+		if issafe(i, col, slc, bslc, rlook, slclook, bslclook):
+			board[i][col] = 1
+			slclook[slc[i][col]] = True
+			bslclook[bslc[i][col]] = True
+			rlook[i] = True
+
+			if panda(board, col + 1, slc, bslc, rlook, slclook, bslclook):
+				return True
+
+			board[i][col] = 0
+			slclook[slc[i][col]] = False
+			bslclook[bslc[i][col]] = False
+			rlook[i] = False
+
+	return False
+
+
+board = [[0 for i in range(N)] for j in range(N)]
+slc = [[0 for i in range(N)] for j in range(N)]
+bslc = [[0 for i in range(N)] for j in range(N)]
+
+rlook = [False] * N
+slclook = [False] * (2 * N + 1)
+bslclook = [False] * (2 * N + 1)
+
+for i in range(N):
+	for j in range(N):
+		slc[i][j] = i + j
+		bslc[i][j] = i - j + N - 1
+
+if not panda(board, 0, slc, bslc, rlook, slclook, bslclook):
+	print("No solution!")
+else:
+	printsoln(board)
